e549d5c0635f74138dc0a12382ef00c618cc064c
Reeya07 <rmohajur7@gmail.com>
2026-05-19 15:37:21 +0400

    f

--- lib/views/emergency.dart
+++ lib/views/emergency.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:mental_health_app/views/chat.dart';
 
 class EmergencyScreen extends StatefulWidget {
   const EmergencyScreen({super.key});
